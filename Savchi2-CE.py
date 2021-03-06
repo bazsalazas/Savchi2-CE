cf5d275c1f1373320f26798d5fde054ceb7fca76
bazsalazas <ancsa.balazs@gmail.com>
2021-03-07 00:18:23 +0100

    download rom download ram and hash save files to PC (to games folder) add games folder to git ignore list

--- Savchi2-CE.py
+++ Savchi2-CE.py
@@ -2,20 +2,19 @@
 import logging
 import logging.config
 import configparser
+import os
 import time
 from ftplib import FTP
 
 from game import Game
 
 # Defines
-LOGGING_CONFIG  = "logging.ini"
-CONFIG_FILE = "config.ini"
-
+LOGGING_CONFIG = "logging.ini"
+CONFIG_FILE    = "config.ini"
 
 # Create logger based on config
 logging.config.fileConfig(LOGGING_CONFIG)
 logger = logging.getLogger("Savchi2 CE")
-
 
 # Welcome message
 logger.info("Savchi2 CE started")
@@ -23,6 +22,10 @@ logger.info("Savchi2 CE started")
 # Load configuration
 config = configparser.ConfigParser()
 config.read(CONFIG_FILE)
+
+# Create game folder if does not exist
+if not os.path.isdir("games"):
+	os.mkdir("games")
 
 # Do main loop
 logger.info("Connecting to FTP server through IP %s", config["FTP"]["IP"])
@@ -45,18 +48,38 @@ while True:
 			# Read game details
 			games = []
 			for gid in gids:
-
-				# Read game ctalog
-				catalog = bytes()
+				# Data read callback
+				data = bytes()
 				def cb(d):
-					global catalog
-					catalog = catalog + d
+					global data
+					data = data + d
+				# Read game catalog
 				ftp.retrbinary("RETR " + config["Games"]["catalog"].format(
 								path=config["Games"]["path"], ID=gid), cb)
 
 				# Create game object
-				games.append(Game(catalog.decode("utf-8")))
-				logger.debug("Game found: '%s'", games[-1].getTitle())
+				game = Game(data.decode("utf-8"))
+				games.append(game)
+				logger.info("Game found: '%s'", game.getTitle())
+
+				# Read game files
+				fd = []
+				for f in game.getConsoleFiles():
+					# Clear buffer
+					data = bytes()
+
+					# Check if game file exists
+					if len(ftp.nlst(f)) == 1:
+						# Read game file
+						logger.debug("Download file: '%s'", f)
+						ftp.retrbinary("RETR " + f, cb)
+
+					# Add to results
+					fd.append(data)
+
+				# Save game files
+				logger.debug("Save game files")
+				game.saveConsoleFiles("games", fd)
 			break
 	
 	# Exit on interruption
--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 # Imports
 import configparser
 import re
+import os
 
 # Class for games
 class Game(object):
@@ -13,14 +14,50 @@ class Game(object):
 		self.config = configparser.ConfigParser(allow_no_value=True)
 		self.config.read_string(desktop_string)
 
-		# Get title
+		# Get id and title
+		self.id = self.config["X-CLOVER Game"]["code"]
 		self.title = self.config["Desktop Entry"]["Name"]
 		
 		# Get ROM path
 		self.rom = re.search("(?<=-rom ).*.sfrom", self.config["Desktop Entry"]["exec"]).group(0)
 
+		# Get RAM path
+		self.ram = self.config["Desktop Entry"]["path"] + "/cartridge.sram"
+		self.ram_hash = self.config["Desktop Entry"]["path"] + "/cartridge.sram.hash"
+
 		#with open('example.ini', 'w') as configfile:
 		#	self.config.write(configfile)	
+
+	""" Get title of the game """
+	def getTitle(self):
+		return self.title
+
+	""" Get list of files on console related to game """
+	def getConsoleFiles(self):
+		# Return rom, ram and ram hasd path
+		return [self.rom, self.ram, self.ram_hash]
+
+	""" Save console files to computer """
+	def saveConsoleFiles(self, dir, flist):
+		# Create game folder if does not exist
+		if not os.path.isdir(dir + "/" + self.id):
+			os.mkdir(dir + "/" + self.id)
+
+		# Save rom
+		if len(flist[0]) > 0:
+			with open(dir + "/" + self.id + "/rom.sfrom", "wb") as f:
+				f.write(flist[0])
+
+		# Save ram
+		if len(flist[1]) > 0:
+			with open(dir + "/" + self.id + "/ram.sram", "wb") as f:
+				f.write(flist[1])
+
+		# Save rom
+		if len(flist[2]) > 0:
+			with open(dir + "/" + self.id + "/ram.sram.hash", "wb") as f:
+				f.write(flist[2])
+		
 
 	""" String representation of the object """
 	def __repr__(self):
